9e209450682a9b7f5b08f1fc1ca7dd9b03d2d1fb
王小错 <eliwang@vip.qq.com>
2020-09-04 17:17:13 +0800

    issue 7531

--- src/pages/information/index.jsx
+++ src/pages/information/index.jsx
@@ -15,9 +15,11 @@ class Index extends Component {
   componentDidHide() {}
 
   render() {
-
+    const text = `正文内容 &nbsp; &nbsp; &nbsp; &nbsp; 正文内容 &nbsp; &nbsp; &nbsp; &nbsp; 正文内容`
     return (
       <View className='index'>
+        {/* TODO: issue: https://github.com/NervJS/taro/issues/7531 */}
+        <View dangerouslySetInnerHTML={{ __html: text }} className='rich-text-container '></View>
       </View>
     )
   }
